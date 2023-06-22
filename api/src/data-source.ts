b604a79e9b436ef552797c339bb30e0edfa4f662
cyan92128505 <cyan92128505@gmail.com>
2023-06-22 08:00:16 +0800

    ✨ feat: Fix database sync

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -2,13 +2,16 @@ require("dotenv").config();
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import config from "config";
+import { User } from "./entity/User";
+import { Migration1687376289528 } from "./migration/1687376289528-migration";
+import { AddedUserEntity1687376325906 } from "./migration/1687376325906-added-user-entity";
 
 export const AppDataSource = new DataSource({
   url: config.get<string>("postgresUrl"),
   type: "postgres",
-  synchronize: false,
+  synchronize: true,
   logging: false,
-  entities: ["src/entity/**/*{.ts,.js}"],
-  migrations: ["src/migration/**/*{.ts,.js}"],
-  subscribers: ["src/subscriber/**/*{.ts,.js}"],
+  entities: [User],
+  migrations: [Migration1687376289528, AddedUserEntity1687376325906],
+  subscribers: [],
 });
